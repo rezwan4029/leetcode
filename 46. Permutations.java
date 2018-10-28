6d0892cc5222959f361076781c405ed11b940e81
Rezwanul Islam <rezwan.maruf.ndc09@gmail.com>
2018-10-28 10:29:56 +0100

    Create 46. Permutations.java

---
+++ 46. Permutations.java
@@ -0,0 +1,30 @@
+class Solution {
+    
+    List<List<Integer>> results;
+    
+    void rec(int[] nums, boolean[] taken, LinkedList<Integer> curr) {
+        
+        if(nums.length == curr.size()) {
+            results.add(curr);
+            return;
+        }
+        
+        for(int i = 0 ; i < nums.length; i++) {
+            if(!taken[i]) {
+                taken[i] = true;
+                LinkedList<Integer> temp = new LinkedList<>(curr);
+                temp.add(nums[i]);
+                rec(nums, taken, temp);
+                taken[i] = false;
+            }
+        }
+    }
+    
+    public List<List<Integer>> permute(int[] nums) {
+        results = new ArrayList<>();
+        boolean taken [] = new boolean[nums.length];
+        rec(nums, taken, new LinkedList<>());
+        return results;
+    }
+    
+}
