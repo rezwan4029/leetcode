734c6e2aa9288aadbfeffaeb413994ec39ef4464
Rezwanul Islam <rezwan.maruf.ndc09@gmail.com>
2018-11-20 14:19:17 +0100

    Update 78. Subsets.java

--- 78. Subsets.java
+++ 78. Subsets.java
@@ -1,6 +1,9 @@
 // https://leetcode.com/problems/subsets/description/
 
 class Solution {
+    
+    // Solution - 1 : Bitmask
+    
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> Ans = new ArrayList<List<Integer>>();
         for(int mask = 0 ; mask < ( 1 << nums.length ); mask++ ){
@@ -14,4 +17,28 @@ class Solution {
         }
         return Ans;
     }
+    
+    // Solution - 2: Backtrack
+    
+    List<List<Integer>> ans;
+    
+    void solve(int pos, List<Integer> taken, int [] nums, int n) {
+        if(pos == n) {
+            ans.add(taken);
+            return;
+        }
+        List<Integer> now = new ArrayList<>(taken);
+        now.add(nums[pos]);
+        solve(pos + 1, now, nums, n);
+        solve(pos + 1, taken, nums, n);
+    }
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        ans = new ArrayList<>();
+        int n = nums.length;
+        solve(0, new ArrayList<>(), nums, n);
+        Collections.reverse(ans);
+        return ans;
+    }
 }
+
