ad3b4881ad8709ad9ce18ca8dcbb6156fb91a3dd
Rezwanul Islam <rezwan.maruf.ndc09@gmail.com>
2018-11-23 22:00:14 +0100

    Update 204. Count Primes.java

--- 204. Count Primes.java
+++ 204. Count Primes.java
@@ -14,4 +14,21 @@ class Solution {
         }
         return ans;
     }
+    
+    
+    public int countPrimes(int n) {
+        boolean prime [] = new boolean[n];
+        for(int i = 0 ; i < n ; i++) prime[i] = true;
+        int ans = 0;
+        int sq = (int)Math.sqrt(n) + 1;
+        for(int i = 2; i < sq ; i++) {
+            if(!prime[i]) continue;
+            for(int j = i << 1 ; j < n ; j += i ) {
+                prime[j] = false;
+            }
+        }
+        for(int i = 2; i < n ; i++) ans += (prime[i] ? 1 : 0);
+        return ans;
+    }
+    
 }
