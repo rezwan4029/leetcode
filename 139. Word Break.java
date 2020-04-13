da2b1a0e5452a9af51ebe48c8bd447f92927263b
Rezwanul Islam <rezwan.maruf.ndc09@gmail.com>
2020-04-13 23:21:07 +0200

    Update 139. Word Break.java

--- 139. Word Break.java
+++ 139. Word Break.java
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/word-break/
 
-class Solution {
+class Solution1 {
     
     public boolean wordBreak(String s, List<String> wordDict) {
         int n = wordDict.size();
@@ -30,3 +30,31 @@ class Solution {
     }
 }
 
+// DP solution
+class Solution2 {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if (s == null || s.length() == 0) {
+            return true;
+        }
+        Set<String> dict = new HashSet<>();
+        for (String word : wordDict) {
+            dict.add(word);
+        }
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            if (dict.contains(s.substring(0, i))) {
+                dp[i] = true;
+                continue;
+            }
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+}
