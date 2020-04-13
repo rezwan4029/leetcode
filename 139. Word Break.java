0904de09b897327d29941a99de433aeb8d877a60
Rezwanul Islam <rezwan.maruf.ndc09@gmail.com>
2020-04-13 22:47:35 +0200

    Update 139. Word Break.java

--- 139. Word Break.java
+++ 139. Word Break.java
@@ -5,17 +5,15 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         int n = wordDict.size();
         Queue<String>q = new LinkedList<>();
-        Collections.sort(wordDict);
-        Collections.reverse(wordDict);
-        HashMap<String, Boolean> seen = new HashMap<>();
         for(int i = 0 ; i < n ; i++) {
             if(s.startsWith(wordDict.get(i))){
                 q.add(wordDict.get(i));
             }
         }
         if(q.isEmpty()) return false;
+        HashMap<String, Boolean> seen = new HashMap<>();
         while(!q.isEmpty()) {
-            String now = q.remove();
+            String now = q.poll();
             if(seen.getOrDefault(now, false)) continue;
             seen.put(now, true);
             if(now.equals(s)) return true;
