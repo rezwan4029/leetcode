5a274c670a0e9312947393fd7d857fdf977aeb38
Rezwanul Islam <rezwan.maruf.ndc09@gmail.com>
2020-07-12 00:52:30 +0200

    Bottom up DP + unique way to travel grid
    A robot is located at the top-left corner of a m x n grid
    The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid.
    How many possible unique paths are there?

--- 62. Unique Paths.java
+++ 62. Unique Paths.java
@@ -5,6 +5,8 @@ class Solution {
         
         int g[][] = new int[m][n];
         
+        g[m - 1][n - 1] = 1;
+        
         for(int i = m - 1; i >= 0; i--)
             for(int j = n - 1; j >= 0; j--) {
                 if(i + 1 < m && j + 1 < n) {
@@ -13,8 +15,6 @@ class Solution {
                      g[i][j] += g[i + 1][j];
                 } else if(j + 1 < n) {
                      g[i][j] += g[i][j + 1];
-                } else {
-                    g[i][j] = 1;
                 }
             }
         return g[0][0];
