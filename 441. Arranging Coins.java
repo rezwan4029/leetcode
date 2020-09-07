96e2da61fa91e8e5fee7d1e3a44c16a60813264d
Rezwanul Islam <rezwan.maruf.ndc09@gmail.com>
2020-09-07 11:40:56 +0200

    Binary Search

--- 441. Arranging Coins.java
+++ 441. Arranging Coins.java
@@ -1,13 +1,19 @@
 // https://leetcode.com/problems/arranging-coins
 
 class Solution {
-    
     public int arrangeCoins(int n) {
-        int ans = 0;
-        for(int pos = 1; n - pos >= 0 ; pos++) {
-            n -= pos;
-            ans++;
+        long lo = 1, hi = n;
+        while(lo <= hi) {
+            long mid = (lo + hi) / 2;
+            long r = (mid * (mid + 1)) / 2;
+            if(r > n) {
+                hi = mid - 1;
+            } else if(r == n) {
+                return (int)mid;
+            } else {
+                lo = mid + 1;
+            }
         }
-        return ans;
+        return (int)hi;
     }
 }
